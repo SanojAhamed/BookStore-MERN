d543ec37fa1f929dbed410ee6cfeeca470b18b7c
Sanoj Ahamed <sanojahamed1730@gmail.com>
2024-09-01 11:18:41 +0530

    feat: Improve User Experience

---
+++ Frontend/src/components/home/BookModal.jsx
@@ -0,0 +1,44 @@
+import { AiOutlineClose } from 'react-icons/ai';
+import { PiBookOpenTextLight } from 'react-icons/pi';
+import { BiUserCircle } from 'react-icons/bi';
+
+const BookModal = ({ book, onClose }) => {
+  return (
+    <div
+      className='fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center'
+      onClick={onClose}
+    >
+      <div
+        onClick={(event) => event.stopPropagation()}
+        className='w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative'
+      >
+        <AiOutlineClose
+          className='absolute right-6 top-6 text-3xl text-red-600 cursor-pointer'
+          onClick={onClose}
+        />
+        <h2 className='w-fit px-4 py-1 bg-red-300 rounded-lg'>
+          {book.publishYear}
+        </h2>
+        <h4 className='my-2 text-gray-500'>{book._id}</h4>
+        <div className='flex justify-start items-center gap-x-2'>
+          <PiBookOpenTextLight className='text-red-300 text-2xl' />
+          <h2 className='my-1'>{book.title}</h2>
+        </div>
+        <div className='flex justify-start items-center gap-x-2'>
+          <BiUserCircle className='text-red-300 text-2xl' />
+          <h2 className='my-1'>{book.author}</h2>
+        </div>
+        <p className='mt-4'>Description</p>
+        <p className='my-2'>
+        In The Silent Observer, Alex Johnson takes readers on a gripping journey through the enigmatic world
+         of espionage and deception. Set in the tumultuous era of the Cold War, the novel follows the life 
+         of an unassuming librarian who discovers a hidden talent for intelligence work. As secrets unravel
+          and alliances shift, the protagonist must navigate a treacherous landscape of betrayal and danger,
+         all while trying to protect a secret that could alter the course of history.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BookModal;
