f6ad7236c9f347641f5188c86ed6e661ae057698
Sanoj Ahamed <sanojahamed1730@gmail.com>
2024-09-05 13:25:37 +0530

    docs: comments pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'; 
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import CreateBooks from './pages/CreateBooks';
-import ShowBook from './pages/ShowBook';
-import EditBook from './pages/EditBook';
-import DeleteBook from './pages/DeleteBook';
+import { Routes, Route } from 'react-router-dom';     // import Routes and Route components from react-router-dom
+import Home from './pages/Home';   // import Home component from pages/Home
+import CreateBooks from './pages/CreateBooks';  // import CreateBooks component from pages/CreateBooks
+import ShowBook from './pages/ShowBook';  // import ShowBook component from pages/ShowBook
+import EditBook from './pages/EditBook'; // import EditBook component from pages/EditBook
+import DeleteBook from './pages/DeleteBook'; // import DeleteBook component from pages/DeleteBook
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/'element={<Home />} />
       <Route path='/books/create' element={<CreateBooks />} />
       <Route path='/books/details/:id' element={<ShowBook />} />
       <Route path='/books/edit/:id' element={<EditBook />} />
