a8952579968c87ffc36012c1f52d92ad0181b6b6
Sanoj Ahamed <sanojahamed1730@gmail.com>
2024-09-05 12:23:53 +0530

    docs: comments home

--- Frontend/src/components/home/BooksCard.jsx
+++ Frontend/src/components/home/BooksCard.jsx
@@ -3,8 +3,9 @@ import { PiBookOpenTextLight } from 'react-icons/pi';
 import { BiUserCircle } from 'react-icons/bi';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineDelete } from 'react-icons/md';
-import BookSingleCard from './BookSingleCard';
+import { MdOutlineDelete } from 'react-icons/md';  // import MdOutlineDelete component from react-icons/md
+import BookSingleCard from './BookSingleCard';   // import BookSingleCard component
+
 
 const BooksCard = ({ books }) => {
   return (
